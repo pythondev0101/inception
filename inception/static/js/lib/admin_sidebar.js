e3c306f5e1b96987612679ab2252f91ffb8f4d91
rmontemayor <36463571+pythondev0101@users.noreply.github.com>
2022-11-01 04:17:29 +0800

    feat: Add mm-active to the current page's <li>

--- inception/static/js/lib/admin_sidebar.js
+++ inception/static/js/lib/admin_sidebar.js
@@ -1,5 +1,10 @@
 $(function() {
-    $(".inception-link").on('click', function(){
-        $this.addClass('mm-active');
+    // Add mm-active to the current page's <li>
+    var activeUrlName = "/" + $(location).attr('href').split("/").splice(3, 4).join("/");
+
+    $(".inception-link").each(function() {
+        if(activeUrlName == $(this).attr('href')){
+            $(this).addClass('mm-active');
+        }
     });
 }); // function
