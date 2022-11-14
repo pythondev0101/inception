42c0774ac88e72dcfcd646f9cf5072b46d919c13
pythondev0101 <rmontemayor0101@gmail.com>
2022-11-14 13:22:29 +0800

    fix: billing url parameter and sidebar js

--- inception/static/js/lib/admin_sidebar.js
+++ inception/static/js/lib/admin_sidebar.js
@@ -1,8 +1,10 @@
 $(function() {
     // Add mm-active to the current page's <li>
-    var activeUrlName = "/" + $(location).attr('href').split("/").splice(3, 4).join("/");
+    var activeUrlName = "/" + $(location).attr('href').split("/").splice(3, 4).join("/").split('?')[0];
 
+    console.log(activeUrlName);
     $(".inception-link").each(function() {
+        console.log($(this).attr('href'));
         if(activeUrlName == $(this).attr('href')){
             $(this).addClass('mm-active');
         }
